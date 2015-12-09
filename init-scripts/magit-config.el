df8e4a57a6a9ee478fab787389618c42c0178132
Guillermo Robles <guillerobles1995@gmail.com>
2015-12-09 13:02:28 +0100

    Change magit-status keybinding to C-c C-g

--- init-scripts/magit-config.el
+++ init-scripts/magit-config.el
@@ -1,9 +1,9 @@
 ;; Configuración particular de magit
-;; Timestamp:<2015-12-08 - 19:38>
+;; Timestamp:<2015-12-09 - 12:50>
 
 ;; (define-prefix-command 'magit-map)
 ;; (global-set-key (kbd "C-M-g") 'magit-map)
 ;; (define-key magit-map (kbd "s") 'magit-status)
 ;; (define-key spellchecking-map (kbd "s s") 'flyspell-mode-es)
-(global-set-key (kbd "C-c g") 'magit-status)
+(global-set-key (kbd "C-c C-g") 'magit-status)
 
