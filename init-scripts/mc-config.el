acf8d15c2b4fbac2dc703e0712ee08194891e518
Guillermo Robles <guillerobles1995@gmail.com>
2015-12-16 17:16:12 +0100

    Add mc-config.el, with its keybindings
    New keybinding: C-c m l -> mc-edit-lines
    New keybinding: C-c m a -> mc-mark-all-dwim
    New keybinding: C-c m A -> mc-mark-all-like-this-dwim

---
+++ init-scripts/mc-config.el
@@ -0,0 +1,6 @@
+;; Configuration of mc
+;; Timestamp:<2015-12-16 - 17:12>
+
+(global-set-key (kbd "C-c m l") 'mc-edit-lines)
+(global-set-key (kbd "C-c m a") 'mc-mark-all-dwim)
+(global-set-key (kbd "C-c m A") 'mc-mark-all-like-this-dwim)
