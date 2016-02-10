e3a1755134e20b8092142132a1ce2f9b6d21164b
Guillermo Robles <guillerobles1995@gmail.com>
2016-02-10 10:41:30 +0100

    Modify emms keybindings

--- init-scripts/emms-config.el
+++ init-scripts/emms-config.el
@@ -1,5 +1,5 @@
-;; File that loads all the files in the directory
-;; Timestamp: <2015-11-24 - 22:29>
+;; File that loads emms configuration
+;; Timestamp: <2016-02-10 - 10:34>
 
 (require 'emms-setup)
 (emms-all)
@@ -7,5 +7,5 @@
 (setq emms-source-file-default-directory "/media/guillermo/WD_wynro/Music/")
 
 ;; (define-prefix-command 'emms-map)
-(global-set-key (kbd "C-M-;") 'emms-playlist-mode-go)
-(global-set-key (kbd "C-M-,") 'emms-add-directory)
+(global-set-key (kbd "C-M-,") 'emms-playlist-mode-go)
+(global-set-key (kbd "C-M-;") 'emms-add-directory)
