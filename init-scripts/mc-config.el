979404a6657c1035b05c687aa70cba3bcb3b47c7
Guillermo Robles <guillerobles1995@gmail.com>
2016-03-05 18:59:37 +0100

    Add more mc commands

--- init-scripts/mc-config.el
+++ init-scripts/mc-config.el
@@ -1,8 +1,9 @@
 ;; Configuration of mc
-;; Timestamp:<2016-01-11 - 10:22>
+;; Timestamp:<2016-03-03 - 15:57>
 
 (global-set-key (kbd "C-c m l") 'mc-edit-lines)
 (global-set-key (kbd "C-c m a") 'mc-mark-all-dwim)
 (global-set-key (kbd "C-c m A") 'mc-mark-all-like-this-dwim)
 (global-set-key (kbd "C-c m m") 'mc-mark-all-like-this)
+(global-set-key (kbd "C-c m n") 'mc-mark-next-like-this)
 (global-set-key (kbd "C-c m M") 'mc-mark-all-like-this)
