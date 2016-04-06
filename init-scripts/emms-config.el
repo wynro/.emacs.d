3d323938a12c82e9b49aee7257fb77f02dbe5afa
Guillermo Robles <guillerobles1995@gmail.com>
2016-04-06 18:20:06 +0200

    Redefine emms keybinding for the new keyboard
    With the new keyboard distribution is more simple this way.
    
    * C-M-; => Enter Emms buffer
    * C-M-: => Add new directory to the current playlist

--- init-scripts/emms-config.el
+++ init-scripts/emms-config.el
@@ -6,5 +6,5 @@
 (setq emms-source-file-default-directory "/media/guillermo/WD_wynro/Music/")
 
 ;; (define-prefix-command 'emms-map)
-(global-set-key (kbd "C-M-,") 'emms-playlist-mode-go)
-(global-set-key (kbd "C-M-;") 'emms-add-directory)
+(global-set-key (kbd "C-;") 'emms-playlist-mode-go)
+(global-set-key (kbd "C-:") 'emms-add-directory)
