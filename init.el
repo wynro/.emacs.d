67a9da7bd9c1b498c48fd8e4d1c2e60b0dc453ce
Guillermo Robles <guillerobles1995@gmail.com>
2016-05-15 18:10:20 +0200

    Change haskell-mode-hook syntax
    * No functional changes

--- init.el
+++ init.el
@@ -75,7 +75,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(haskell-mode-hook (quote (turn-on-haskell-indent)))
+ '(haskell-mode-hook (quote (turn-on-haskell-indent)) t)
  '(inhibit-startup-screen t)
  ;; '(initial-buffer-choice (concat emacs.d "init-buffer.org"))
  '(send-mail-function (quote mailclient-send-it)))
