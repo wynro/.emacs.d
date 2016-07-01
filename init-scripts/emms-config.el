fb506b7f344a605fa4fbb44599600340b81df46c
Guillermo Robles <guillerobles1995@gmail.com>
2016-07-01 23:37:43 +0200

    Change emms keybindings
    * Apparently the old ones where used by flyspell

--- init-scripts/emms-config.el
+++ init-scripts/emms-config.el
@@ -7,5 +7,5 @@
 (setq emms-source-file-default-directory "/media/guillermo/WD_wynro/Music/")
 
 ;; (define-prefix-command 'emms-map)
-(global-set-key (kbd "C-;") 'emms-playlist-mode-go)
-(global-set-key (kbd "C-:") 'emms-add-directory)
+(global-set-key (kbd "C-'") 'emms-playlist-mode-go)
+(global-set-key (kbd "C-\"") 'emms-add-directory)
