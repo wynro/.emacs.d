0b1ab8a4088f48d6cf45e6750b762a8534c1fa7f
Guillermo Robles <guillerobles1995@gmail.com>
2016-10-02 12:54:38 +0200

    Add uniquify functions

--- init-scripts/functions.el
+++ init-scripts/functions.el
@@ -56,3 +56,19 @@ Version 2015-06-08"
     (insert *string)
     (asciify-text (point-min) (point-max))
     (buffer-string)))
+
+(defun uniquify-all-lines-region (start end)
+  "Find duplicate lines in region START to END keeping first occurrence."
+  (interactive "*r")
+  (save-excursion
+    (let ((end (copy-marker end)))
+      (while
+          (progn
+            (goto-char start)
+            (re-search-forward "^\\(.*\\)\n\\(\\(.*\n\\)*\\)\\1\n" end t))
+        (replace-match "\\1\n\\2")))))
+
+(defun uniquify-all-lines-buffer ()
+  "Delete duplicate lines in buffer and keep first occurrence."
+  (interactive "*")
+  (uniquify-all-lines-region (point-min) (point-max)))
