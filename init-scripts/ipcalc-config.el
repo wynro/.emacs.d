0827d30c21cfe70602e07db550de5a82e8059034
Guillermo Robles <guillerobles1995@gmail.com>
2016-10-04 11:16:53 +0200

    ipcalc: Add conversion between ip/mask and ip/CIDR

--- init-scripts/ipcalc-config.el
+++ init-scripts/ipcalc-config.el
@@ -1,2 +1,29 @@
 (add-to-list 'load-path (concat init-scripts "ipcalc/"))
 (load "ipcalc.el")
+(require 'cl-lib)
+
+(defun ipcalc-from-cidr-to-ipmask (ip/cidr)
+  "Converts from IP/CIDR notation to IP/MASK"
+  (let
+      ((ip (car (split-string ip/cidr "/")))
+       (cidr (string-to-number (car (cdr (split-string ip/cidr "/")))))
+       (mask (ipcalc-binary-to-ip (ipcalc-ones-and-pad cidr)))
+       )
+    (concat ip "/" mask)
+    )
+  )
+
+(defun ipcalc-from-ipmask-to-cidr (ip/mask)
+  "Converts from IP/MASK notation to IP/CIDR"
+  (let
+      ((ip (car (split-string ip/mask "/")))
+       (mask-binary
+        (ipcalc-octets-as-binary (ipcalc-ip-to-octets
+                                  (car (cdr (split-string ip/mask "/")))))
+        )
+       (ip-cidr
+        (- (cl-reduce '+ mask-binary)
+           (* (cl-reduce '+ "0") (length mask-binary)))))
+    (concat ip "/" (number-to-string ip-cidr))
+    )
+  )
