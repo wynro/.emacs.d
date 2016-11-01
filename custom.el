92ef773f4a6c30bb9c620bc3c6d36c30f1d3ef38
Guillermo Robles <guillerobles1995@gmail.com>
2016-11-01 18:06:14 +0100

    Move Custom config to its own file

---
+++ custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(haskell-mode-hook (quote (turn-on-haskell-indent)) t)
+ '(inhibit-startup-screen t)
+ '(package-selected-packages
+   (quote
+    (auto-yasnippet bash-completion circe clips-mode common-lisp-snippets erlang iedit js2-mode magit markdown-mode multi-web-mode nodejs-repl nyan-mode paredit php-mode puppet-mode scala-mode smooth-scroll undo-tree mc-extras emms haskell-mode projectile dockerfile-mode yaml-mode color-theme-modern company)))
+ '(send-mail-function (quote mailclient-send-it))
+ '(use-minimal-error-page (quote on))
+ '(use-minimal-welcome-page (quote on)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
