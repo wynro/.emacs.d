7bb6631e044c0e32b3971a478daf863cf79133db
Guillermo Robles <guillerobles1995@gmail.com>
2016-12-05 12:17:21 +0100

    Remove useless config

--- init-scripts/production-code.el
+++
@@ -1,9 +0,0 @@
-;; Functions to translate from development code to production code
-
-(defun delete-production-code ()
-  "Delete all lines containing the string \"<!PROD\""
-  (interactive)
-  (flush-lines "<!PROD")
-)
-
-;; (global-set-key (kbd "C-x e") 'delete-production-code)
