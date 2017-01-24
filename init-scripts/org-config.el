57a84f5db05217a4875c001fe62cb37ff4aa94ba
Guillermo Robles <guillerobles1995@gmail.com>
2017-01-24 23:11:10 +0100

    Configured org-babel
    - God bless BEGIN_SRC blocks

--- init-scripts/org-config.el
+++ init-scripts/org-config.el
@@ -10,5 +10,14 @@
 ;; C-e c for capture
 (global-set-key (kbd "C-c c") 'org-capture)
 
+(org-babel-do-load-languages 'org-babel-load-languages
+                             '(
+                               (shell . t)
+                               (ruby . t)
+                               (python . t)
+                               )
+                             )
+(setq org-src-tab-acts-natively t)      ;; Activate tabs in BEGIN_SRC blocks
+
 (provide 'org-config)
 ;;; org-config.el ends here
