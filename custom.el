a2dca0c4a5e293e816f8c8c90ef3ead66cfda959
Guillermo Robles <guillerobles1995@gmail.com>
2017-02-23 00:15:47 +0100

    Add powershell-mode package

--- custom.el
+++ custom.el
@@ -12,7 +12,7 @@
  '(minimap-window-location (quote right))
  '(package-selected-packages
    (quote
-    (systemd org minimap graphviz-dot-mode forth-mode tf2-conf-mode csgo-conf-mode package-lint achievements flycheck auctex auto-yasnippet bash-completion clips-mode color-theme-modern common-lisp-snippets company csv-mode dockerfile-mode elnode emms erlang gnuplot haskell-mode iedit js2-mode lua-mode magit markdown-mode mc-extras multi-web-mode multiple-cursors nodejs-repl nyan-mode page-break-lines paredit php-mode projectile puppet-mode scala-mode smooth-scroll undo-tree web-server yaml-mode)))
+    (powershell systemd org minimap graphviz-dot-mode forth-mode tf2-conf-mode csgo-conf-mode package-lint achievements flycheck auctex auto-yasnippet bash-completion clips-mode color-theme-modern common-lisp-snippets company csv-mode dockerfile-mode elnode emms erlang gnuplot haskell-mode iedit js2-mode lua-mode magit markdown-mode mc-extras multi-web-mode multiple-cursors nodejs-repl nyan-mode page-break-lines paredit php-mode projectile puppet-mode scala-mode smooth-scroll undo-tree web-server yaml-mode)))
  '(send-mail-function (quote mailclient-send-it))
  '(use-minimal-error-page (quote on))
  '(use-minimal-welcome-page (quote on)))
