964dc617433f383687ab96cfcccd05940e9eba21
Guillermo Robles <guillerobles1995@gmail.com>
2017-02-23 00:17:46 +0100

    Add media keys support

--- init-scripts/emms-config.el
+++ init-scripts/emms-config.el
@@ -11,5 +11,19 @@
 (global-set-key (kbd "C-'") 'emms-playlist-mode-go)
 (global-set-key (kbd "C-\"") 'emms-add-directory)
 
+(global-set-key [C-XF86AudioPlay] 'emms-pause)
+(global-set-key [C-XF86AudioStop] 'emms-stop)
+(global-set-key [C-XF86AudioNext] 'emms-next)
+(global-set-key [C-XF86AudioPrev] 'emms-previous)
+
+(defun nop ()
+  "Does nothing."
+  (interactive)
+  )
+;; This is a terrible hack, but apparently Emacs interprets Control+Fn
+;; as [C-XF86WakeUp], so I add this to silence the messages of not
+;; bounded
+(global-set-key [C-XF86WakeUp] 'nop)
+
 (provide 'emms-config)
 ;;; emms-config.el ends here
