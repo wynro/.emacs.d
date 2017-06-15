74b2cf1494a22b4fb3c2594275db0aad6d78f109
Guillermo Robles <guillerobles1995@gmail.com>
2017-06-15 10:24:12 +0200

    Add non-regex aware search binding

--- init-scripts/isearch.el
+++ init-scripts/isearch.el
@@ -6,6 +6,8 @@
 ;; Change the default search system with one that uses regexp
 (global-set-key (kbd "C-r") 'isearch-backward-regexp)
 (global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "C-M-s") 'isearch-forward)
+(global-set-key (kbd "C-M-r") 'isearch-backward)
 
 (provide 'isearch)
 ;;; isearch.el ends here
