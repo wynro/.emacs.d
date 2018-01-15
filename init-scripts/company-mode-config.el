1e1745d8e3541917f4dc0349c4a12651de714b8d
Guillermo Robles <guillerobles1995@gmail.com>
2018-01-15 11:48:14 +0100

    Move company configuration to use-package

--- init-scripts/company-mode-config.el
+++ init-scripts/company-mode-config.el
@@ -3,9 +3,18 @@
 ;;;
 ;;; Code:
 
-(require 'company)
-(global-company-mode t)
-(setq company-idle-delay 0)
+(use-package company
+  :ensure t
+  :config
+  (global-company-mode t)
+  (setq company-idle-delay 0)
+  )
+
+(use-package company-statistics
+  :ensure t
+  :config
+  (add-hook 'after-init-hook 'company-statistics-mode)
+  )
 
 (provide 'company-mode-config)
 ;;; company-mode-config.el ends here
