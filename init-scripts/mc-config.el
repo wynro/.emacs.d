ac1f50652c73880088a0200b0c835ef27a006735
Guillermo Robles <guillerobles1995@gmail.com>
2018-01-15 11:21:48 +0100

    Move mc configuration to use-package

--- init-scripts/mc-config.el
+++ init-scripts/mc-config.el
@@ -3,12 +3,16 @@
 ;;;
 ;;; Code:
 
-(global-set-key (kbd "C-c m l") 'mc/edit-lines)
-(global-set-key (kbd "C-c m a") 'mc/mark-all-dwim)
-(global-set-key (kbd "C-c m A") 'mc/mark-all-like-this-dwim)
-(global-set-key (kbd "C-c m m") 'mc/mark-all-like-this)
-(global-set-key (kbd "C-c m n") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-c m M") 'mc/mark-all-like-this)
+(use-package multiple-cursors
+  :ensure t
+  :bind (
+         ("C-c m l" . mc/edit-lines)
+         ("C-c m a" . mc/mark-all-dwim)
+         ("C-c m A" . mc/mark-all-like-this-dwim)
+         ("C-c m m" . mc/mark-all-like-this)
+         ("C-c m n" . mc/mark-next-like-this)
+         ))
+
 
 (provide 'mc-config)
 ;;; mc-config.el ends here
