c55052dce44c48526fbdd91a78181d6ae8f5a517
Guillermo Robles <guillerobles1995@gmail.com>
2018-01-15 11:19:57 +0100

    Move tetris configuration to use-package

--- init-scripts/tetris-config.el
+++ init-scripts/tetris-config.el
@@ -3,18 +3,23 @@
 ;;;
 ;;; Code:
 
-(require 'tetris)
-(setq tetris-score-file "~/.emacs.d/games/tetris-scores")
-
-(global-set-key (kbd "C-c t") 'tetris)
-(define-key tetris-mode-map "n"     'tetris-start-game)
-(define-key tetris-mode-map "q"     'tetris-end-game)
-(define-key tetris-mode-map "p"     'tetris-pause-game)
-(define-key tetris-mode-map "\r"    'tetris-move-bottom)
-(define-key tetris-mode-map [left]  'tetris-move-left)
-(define-key tetris-mode-map [right] 'tetris-move-right)
-(define-key tetris-mode-map [up]    'tetris-rotate-prev)
-(define-key tetris-mode-map [down]  'tetris-rotate-next)
+(use-package tetris
+  :ensure t
+  :config
+  (setq tetris-score-file "~/.emacs.d/games/tetris-scores")
+  ;; (defun tetris()"DONT PLAY."(interactive)(message "DONT PLAY"))
+  :bind (("C-c t" . tetris))
+  :bind (:map tetris-mode-map
+              ("n"     . tetris-start-game)
+              ("q"     . tetris-end-game)
+              ("p"     . tetris-pause-game)
+              ("\r"    . tetris-move-bottom)
+              ([left]  . tetris-move-left)
+              ([right] . tetris-move-right)
+              ([up]    . tetris-rotate-prev)
+              ([down]  . tetris-rotate-next)
+              )
+  )
 
 (provide 'tetris-config)
 ;;; tetris-config.el ends here
