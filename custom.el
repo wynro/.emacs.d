7c0ee7012d1bc8f9bc2649476584731a03f92934
Guillermo Robles <guillerobles1995@gmail.com>
2021-02-10 20:01:22 +0100

    Remove old custom options, add new packages

--- custom.el
+++ custom.el
@@ -3,23 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("5eb4b22e97ddb2db9ecce7d983fa45eb8367447f151c7e1b033af27820f43760" default)))
- '(haskell-mode-hook (quote (turn-on-haskell-indent)) t)
- '(inhibit-startup-screen t)
- '(initial-major-mode (quote org-mode))
- '(initial-scratch-message "# Scratch
-")
- '(magit-branch-arguments nil)
- '(magit-commit-arguments nil)
  '(package-selected-packages
-   (quote
-    (company-statistics company-ansible ansible-vault ansible-doc smartparens discover typing evil ipcalc column-marker cobol-mode dokuwiki-mode suggest go-guru go-autocomplete go-mode powershell systemd org graphviz-dot-mode forth-mode tf2-conf-mode csgo-conf-mode package-lint achievements flycheck auctex auto-yasnippet bash-completion clips-mode color-theme-modern common-lisp-snippets company csv-mode dockerfile-mode elnode erlang gnuplot haskell-mode iedit js2-mode lua-mode magit markdown-mode mc-extras multi-web-mode multiple-cursors nodejs-repl page-break-lines php-mode projectile puppet-mode scala-mode undo-tree web-server yaml-mode)))
- '(safe-local-variable-values (quote ((TeX-master . auto))))
- '(send-mail-function (quote mailclient-send-it))
- '(use-minimal-error-page (quote on))
- '(use-minimal-welcome-page (quote on)))
+   '(ace-jump-mode achievements ansible ansible-doc ansible-vault arduino-mode auctex auto-yasnippet bash-completion clips-mode cobol-mode color-theme-modern column-marker common-lisp-snippets company-ansible company-go company-statistics company-terraform csgo-conf-mode csharp-mode csv-mode diminish discover docker docker-compose-mode dockerfile-mode dokuwiki-mode eldoc electric-operator elpy emms emojify erlang evil exec-path-from-shell find-file-in-project flycheck folding forth-mode gnuplot go-dlv go-guru graphviz-dot-mode groovy-imports groovy-mode haskell-mode highlight-chars htmlize iedit ipcalc js2-mode kubernetes love-minor-mode lsp-mode lsp-ui markdown-mode mc-extras multi-web-mode nginx-mode no-littering nodejs-repl org package-lint page-break-lines php-mode powershell projectile protobuf-mode puppet-mode python python-black rustic scala-mode smartparens smooth-scrolling sql-indent suggest systemd terraform-doc tf2-conf-mode timonier undo-tree use-package vimrc-mode web-server weblorg yasnippet-snippets yasnippet-snippets)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
